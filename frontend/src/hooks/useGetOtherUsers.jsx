b2fb11e95c90be4bc045c1879ee8b1a6ebfcf3a3
Manas Singh <131545755+Manask76@users.noreply.github.com>
2024-08-12 01:05:21 +0530

    Update useGetOtherUsers.jsx

--- frontend/src/hooks/useGetOtherUsers.jsx
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -1,27 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
-import { BASE_URL } from '..';
-import { setOtherUsers } from '../components/redux/userSlice';
+import { BASE_URL } from "..";
+import { setOtherUsers } from "../components/redux/userSlice";
 
 const useGetOtherUsers = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const fetchOtherUsers = async () => {
-            try {
-                axios.defaults.withCredentials = true;
-                const res = await axios.get(`${BASE_URL}/api/v1/user`);
-                // store
-                console.log("other users -> ",res);
-                dispatch(setOtherUsers(res.data));
-            } catch (error) {
-                console.log(error);
-            }
+  useEffect(() => {
+    const fetchOtherUsers = async () => {
+      try {
+        axios.defaults.withCredentials = true;
+        const token = localStorage.getItem("token");
+        if (token) {
+          const res = await axios.get(`${BASE_URL}/api/v1/user`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          // store
+          console.log("other users -> ", res);
+          dispatch(setOtherUsers(res.data));
         }
-        fetchOtherUsers();
-    }, [])
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchOtherUsers();
+  }, [dispatch]); // Add dispatch to the dependency array
+};
 
-}
-
-export default useGetOtherUsers
+export default useGetOtherUsers;
